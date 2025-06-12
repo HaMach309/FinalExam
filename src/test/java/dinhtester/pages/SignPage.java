1e44fc11ac7f503777e6fae2410f83f662d96035
DinhPX <xuandinhpham92@gmail.com>
2025-06-13 00:57:54 +0700

    first commit maven 3

--- src/test/java/dinhtester/pages/SignPage.java
+++ src/test/java/dinhtester/pages/SignPage.java
@@ -34,7 +34,7 @@ public class SignPage {
         validateHelper.setText(passwordInput, password);
         validateHelper.clickElement(loginSubmitBtn);
         Thread.sleep(3000);
-        System.out.println("try maven jenskin 2");
+        System.out.println("try maven jenskin 3");
         return new DashboardPage(driver);
     }
 }
